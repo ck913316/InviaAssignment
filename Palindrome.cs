50ec0f8a720f5ecc113493fe2095b181d58672d7
ck913316 <125851465+ck913316@users.noreply.github.com>
2023-05-05 00:33:03 +0530

    Add files via upload

---
+++ Palindrome.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Palindrome
+{
+       public void IsPalindrome(String str)
+       {
+          string rev = "";
+          for(int i=str.Length-1; i>=0; i--) 
+          {
+            rev += str[i];
+          }
+          if (rev == str)
+             Console.WriteLine("Palindrome");
+          else
+             Console.WriteLine("Is not Palindrome");
+       }
+    static void Main()
+    {
+        Console.WriteLine("Enter the words");
+        string word=Console.ReadLine();
+        Palindrome p=new Palindrome();
+        p.IsPalindrome(word);
+     
+    }
+}
